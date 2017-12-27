649915f7cef456537de9dc01a622bcf5fbfe8460
Paris Kasidiaris <paris@sourcelair.com>
2017-12-27 19:35:05 +0200

    Implement GET API endpoints for routes

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
+import os
+
+import requests
 from flask import Flask
+from flask import json
 from flask import render_template
+from flask.views import MethodView
+
+
+CERYX_API_HOST = os.getenv('CERYX_API_HOST')
 
 app = Flask(__name__)
 
@@ -8,6 +16,48 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
-@app.route('/api/routes')
-def routes():
-    return 'Hello World!'
+
+class RoutesListAPI(MethodView):
+    def get(self, route_id=None):
+        if route_id:
+            response = requests.get(f'{CERYX_API_HOST}/api/routes/{route_id}')
+            response.raise_for_status()
+            route_list = response.json()
+            return json.jsonify(**route_list)
+
+        response = requests.get(f'{CERYX_API_HOST}/api/routes')
+        response.raise_for_status()
+        route_list = response.json()
+        return json.jsonify(*route_list)
+
+    def post(self):
+        pass
+
+
+class RoutesDetailAPI(MethodView):
+    def get(self, route_id):
+        response = requests.get(f'{CERYX_API_HOST}/api/routes/{route_id}')
+        response.raise_for_status()
+        route_list = response.json()
+        return json.jsonify(**route_list)
+
+    def put(self, route_id):
+        pass
+
+    def delete(self, route_id):
+        pass
+
+routes_list_view = RoutesListAPI.as_view('routes_list_view')
+routes_detail_view = RoutesDetailAPI.as_view('routes_detail_view')
+
+app.add_url_rule(
+    '/api/routes/',
+    view_func=routes_list_view,
+    methods=['GET', 'POST'],
+)
+
+app.add_url_rule(
+    '/api/routes/<route_id>/',
+    view_func=routes_detail_view,
+    methods=['GET', 'PUT', 'DELETE'],
+)
