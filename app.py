4a04391796b3192eec0cdf58265fd6695aa48402
Paris Kasidiaris <paris@sourcelair.com>
2017-12-28 21:27:08 +0200

    Polish

--- app.py
+++ app.py
@@ -15,6 +15,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
+    """
+    Return the home page (dashboard) of Ceryx Web.
+    """
     return render_template('index.html')
 
 
@@ -22,12 +25,18 @@ class RoutesListAPI(MethodView):
     endpoint = f'{CERYX_API_HOST}/api/routes'
 
     def get(self):
+        """
+        Return the list of all routes registered in Ceryx.
+        """
         response = requests.get(self.endpoint)
         response.raise_for_status()
         route_list = response.json()
         return json.jsonify(*route_list)
 
     def post(self):
+        """
+        Create a new route in Ceryx.
+        """
         response = requests.post(self.endpoint, json=request.json)
         response.raise_for_status()
         route = response.json()
@@ -35,17 +44,12 @@ class RoutesListAPI(MethodView):
 
 
 class RoutesDetailAPI(MethodView):
-    def get(self, route_id):
-        response = requests.get(f'{CERYX_API_HOST}/api/routes/{route_id}')
-        response.raise_for_status()
-        route_list = response.json()
-        return json.jsonify(**route_list)
-
-    def put(self, route_id):
-        pass
-
-    def delete(self, route_id):
-        response = requests.delete(f'{CERYX_API_HOST}/api/routes/{route_id}')
+    def delete(self, route_source):
+        """
+        Delete the route identified by the given route source in Ceryx.
+        """
+        url = f'{CERYX_API_HOST}/api/routes/{route_source}'
+        response = requests.delete(url)
         response.raise_for_status()
         return json.jsonify(), 204
 
@@ -59,7 +63,7 @@ app.add_url_rule(
 )
 
 app.add_url_rule(
-    '/api/routes/<route_id>/',
+    '/api/routes/<route_source>/',
     view_func=routes_detail_view,
-    methods=['GET', 'PUT', 'DELETE'],
+    methods=['DELETE'],
 )
--- static/js/components/Bootstrap.jsx
+++ static/js/components/Bootstrap.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 
+/**
+ * This is a shorthand component for creating Boostrap 4 Alerts.
+ * More info about Bootstrap 4 Alerts: https://getbootstrap.com/docs/4.0/components/alerts/
+ */
 export class Alert extends React.PureComponent {
   render() {
     const dismissible = (this.props.dismissible) ? 'alert-dismissible fade show' : '';
